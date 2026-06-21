356438b4aefc810ef86b3b49dc5a37be9e645d6b
BattenaMaheswari <battenamaheswari123@gmail.com>
2026-06-21 14:05:37 +0530

    Connect Render PostgreSQL database

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -1,9 +1,16 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Bloomeher123@localhost:5432/bloomher_db"
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(
+    DATABASE_URL
+)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -11,12 +18,16 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
+
 Base = declarative_base()
 
 
 def get_db():
+
     db = SessionLocal()
+
     try:
         yield db
+
     finally:
         db.close()
