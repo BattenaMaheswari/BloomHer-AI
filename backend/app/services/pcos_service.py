c429035a524786536f341cbc7a582ca7617e999b
BattenaMaheswari <battenamaheswari123@gmail.com>
2026-06-21 12:02:51 +0530

    Fix PCOS model path for Render

--- backend/app/services/pcos_service.py
+++ backend/app/services/pcos_service.py
@@ -1,15 +1,32 @@
+import os
 import joblib
 import numpy as np
 
 from app.database.models import PCOSPrediction
 
-model = joblib.load("ml/pcos_model.pkl")
+
+# Get correct path for Render + local
+BASE_DIR = os.path.dirname(
+    os.path.dirname(
+        os.path.dirname(__file__)
+    )
+)
+
+MODEL_PATH = os.path.join(
+    BASE_DIR,
+    "ml",
+    "pcos_model.pkl"
+)
+
+
+model = joblib.load(MODEL_PATH)
+
 
 
 def predict_pcos_risk(data, db):
 
     try:
-        # input features
+
         features = np.array([[
             data.age,
             data.bmi,
@@ -21,30 +38,45 @@ def predict_pcos_risk(data, db):
             data.hair_growth
         ]])
 
-        # prediction
+
         prediction = model.predict(features)[0]
+
         probability = model.predict_proba(features)[0]
 
-        confidence = round(float(max(probability)) * 100, 2)
 
-        risk = "High Risk" if prediction == 1 else "Low Risk"
+        confidence = round(
+            float(max(probability)) * 100,
+            2
+        )
 
-        # save DB
+
+        risk = (
+            "High Risk"
+            if prediction == 1
+            else "Low Risk"
+        )
+
+
         new_prediction = PCOSPrediction(
             risk=risk,
             confidence=confidence
         )
 
+
         db.add(new_prediction)
         db.commit()
         db.refresh(new_prediction)
+
 
         return {
             "risk": risk,
             "confidence": confidence
         }
 
+
+
     except Exception as e:
+
         return {
             "error": str(e)
         }
