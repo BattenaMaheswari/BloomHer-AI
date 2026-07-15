af4fba93f1e62129e17407cba0854b8cee41d47f
BattenaMaheswari <battenamaheswari123@gmail.com>
2026-07-15 16:40:48 +0530

    Updated responsive home page with correct image name

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -124,7 +124,7 @@ export default function Home() {
           <div className="flex justify-center">
 
             <Image
-              src="/images/bloomher.jpg"
+              src="/images/bloomeherai.jpg"
               alt="BloomHer AI"
               width={550}
               height={550}
